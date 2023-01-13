7ce93361fa8b81a2906d3029ccb4925b19a0a179
Ann S <gb9363@mail.ru>
2023-01-13 19:43:36 +0300

    Задача 54

---
+++ CL8_Home54/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(String.Format("{0,2:0.#}", arr[i, j]));
+        Console.WriteLine();
+    }
+}
+
+int[,] arr =
+{
+    { 1, 4, 7, 2 },
+    { 5, 9, 2, 3 },
+    { 8, 4, 2, 4 }
+};
+Console.WriteLine("Массив:");
+PrintArray(arr);
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    int[] row = Enumerable.Range(0, arr.GetUpperBound(1)+1).Select(x => arr[i, x]).ToArray();
+    Array.Sort(row);
+    Array.Reverse(row);
+    for (int j = 0; j < row.Length; j++)
+        arr[i, j] = row[j];
+}
+
+Console.WriteLine("Упорядоченный массив:");
+PrintArray(arr);
